616b58030266b24dded9e629b52904157fca6bcd
tiwat posri <69070123+jame12945@users.noreply.github.com>
2022-10-19 15:05:31 +0700

    Add files via upload

---
+++ lsp/Main.java
@@ -0,0 +1,22 @@
+package Lsp;
+
+public class Main {
+    private static final int width = 4;
+    private static final int height = 5;
+
+    public static void main(String[] args) { //Change Object Rectangle
+        Rectangle reg1 = new Rectangle(width, height);
+        System.out.println("==== Rectangle ====");
+        System.out.println("Width = " + width);
+        System.out.println("Height = " + height);
+        System.out.println("Area = " + reg1.getArea());
+
+        Square reg2 = new Square(5); // set to side of square
+
+        reg2.setSide(5); // set to new side of square
+        System.out.println("==== Square ====");
+        System.out.println("Width = " + width);
+        System.out.println("Height = " + height);
+        System.out.println("Area = " + reg2.getArea());
+    }
+}
---
+++ lsp/Square.java
@@ -0,0 +1,15 @@
+package Lsp;
+
+public class Square extends Rectangle {
+    private int side; 
+    Square(int side) {
+        this.setSide(side);
+    }
+    public void setSide(int side) {
+    	this.side = side; 
+    }
+    @Override 
+    public int getArea(){ 
+        return side * side;
+    }
+}
